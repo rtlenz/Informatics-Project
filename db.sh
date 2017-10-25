331dc4e2ae475b405370951050170e7903eb9936
rtlenz <33075694+rtlenz@users.noreply.github.com>
2017-10-24 20:01:11 -0500

    Add files via upload

---
+++ db.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Change DBPASSWORD, DBUSER, DBHOST and DBNAME to match the values
+# your mysql_db_info file on the webdev server.
+mysql --password='t5UshUK3vQ-m' --user='rtlenz' --host='dbdev.cs.uiowa.edu' 'db_rtlenz'
