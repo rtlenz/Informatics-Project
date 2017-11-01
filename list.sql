7c382bc0062a3f7c93d9befce8579b6c80398137
Richard Lenz <richard-lenz@uiowa.edu>
2017-10-31 19:14:30 -0500

    test

--- list.sql
+++ list.sql
@@ -1,1 +1,29 @@
-asdlkajsdlkajs 
+-- drop the table if it already exists
+DROP TABLE IF EXISTS attribute;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS list;
+
+-- this is the table for lists
+CREATE TABLE list (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(120) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE item (
+    id INT NOT NULL AUTO_INCREMENT,
+    list_id INT NOT NULL,
+    name VARCHAR(120) NOT NULL,
+    ordernumber INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE attribute (
+    id INT NOT NULL AUTO_INCREMENT,
+    item_id INT NOT NULL,
+    ordernumber INT NOT NULL,
+    label VARCHAR(120) NOT NULL,
+    type VARCHAR(120) NOT NULL,
+    value VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)    
+);
