0195b908b5138394bb63fe8d1ad54854dc1b3e9b
Richard Lenz <richard-lenz@uiowa.edu>
2017-11-02 13:14:19 -0500

    update

--- getlists.php
+++ getlists.php
@@ -16,43 +16,45 @@
     // assign results to array
     $lists = array();
     $i = 0;
-    while ($list = nextTuple($result)) {
-        $lists[$i] = $list;
-        
-        $listid = $list['id'];
-        
-        // now get the items under the current list
-        $query = "SELECT * FROM item WHERE list_id = $listid ORDER BY ordernumber";
-        
-        // run the query
-        $result_item = queryDB($query, $db);
-        $items = array();
-        $j = 0;
-        while ($item = nextTuple($result_item)) {
-            $items[$j] = $item;
-            
-            $itemid = $item['id'];
-            
-            // now get the attributes for this item
-            $query = "SELECT * FROM attribute WHERE item_id = $itemid ORDER BY ordernumber";
-            
-            //run the query
-            $result_attribute = queryDB($query, $db);
-            $attributes = array();
-            $k = 0;
-            while ($attribute = nextTuple($result_attribute)) {
-                $attributes[$k] = $attribute;
-                $k++;
-            }
-            
-            $items[$j]['attributes'] = $attributes;
-            $j++;
-        }
-        
-        $lists[$i]['items'] = $items;
-        
-        $i++;
-    }
+	if($isComplete){
+		while ($list = nextTuple($result)) {
+			$lists[$i] = $list;
+			
+			$listid = $list['id'];
+			
+			// now get the items under the current list
+			$query = "SELECT * FROM item WHERE list_id = $listid ORDER BY ordernumber";
+			
+			// run the query
+			$result_item = queryDB($query, $db);
+			$items = array();
+			$j = 0;
+			while ($item = nextTuple($result_item)) {
+				$items[$j] = $item;
+				
+				$itemid = $item['id'];
+				
+				// now get the attributes for this item
+				$query = "SELECT * FROM attribute WHERE item_id = $itemid ORDER BY ordernumber";
+				
+				//run the query
+				$result_attribute = queryDB($query, $db);
+				$attributes = array();
+				$k = 0;
+				while ($attribute = nextTuple($result_attribute)) {
+					$attributes[$k] = $attribute;
+					$k++;
+				}
+				
+				$items[$j]['attributes'] = $attributes;
+				$j++;
+			}
+			
+			$lists[$i]['items'] = $items;
+			
+			$i++;
+		}
+	}else{
     
     // put together JSON object to send back
     // send response back
@@ -61,4 +63,5 @@
     $response['value'] = $lists;
     header('Content-Type: application/json');
     echo(json_encode($response));
+	}
 ?>
