abfe8aca814e70dc242b4e555df444ad0e530c02
Richard Lenz <richard-lenz@uiowa.edu>
2017-12-03 16:01:25 -0600

    s

--- vote.php
+++ vote.php
@@ -4,7 +4,7 @@
 		
 
 	$data =json_decode(file_get_contents('php://input'), true);
-	//$voteCount = $data['voteCount'];
+	//$count = $data['voteCount'];
 	
 	
 	//connect to database
@@ -21,31 +21,47 @@
 	
 	if ($isComplete){
 		//everything works
-	
-		 
+		
 		session_start();
-		$listid = $_SESSION['listid'];
+		$list_id = $_SESSION['list_id'];
 		$count = $_SESSION['voteCount'];
 		$accountid = $_SESSION['accountid'];
 		
         $count++; 
 		//make insert statement	
-		$query = "INSERT INTO vote(list_id,voteCount,accountid) VALUES ($listid,$count,$accountid)";
+		$query = "INSERT INTO vote(list_id,voteCount,accountid) VALUES ($list_id,$count,$accountid)";
 		$_SESSION['voteCount'] = $count;
 			
 		//run insert statement
 		$result = queryDB($query,$db);
 		
+	
+	
+		//send a response back to the called of this php file
+		$response =array();
+		$response['status'] = 'success';
+		$response['id'] = $id;
+		header('Content-Type: application/json');
+		echo(json_encode($response));
 		
-		////get id for players just entered Add this to a $session variable with the list id
-		//$_SESSION['item_id'] = mysqli_insert_id($db);
-		//$item_id = $_SESSION['item_id'];
+	} else{
+		//generates an error message to send back to caller
+		ob_start();
+		var_dump($data);
+		$postdump = ob_get_clean();
 		
-		
-		
-		
-		
-		
+		$response = array();
+		$response['status'] = 'error';
+		$response['message'] = $errorMessage . $postdump;
+		header('Content-Type: application/json');
+		echo(json_encode($response));
+	}
+	
+	
+
+?>
+
+
 		
 		//send a response back to the called of this php file
 		$response =array();
