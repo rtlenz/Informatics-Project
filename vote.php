cc5d0daa8502fc785f679f065986c9f7cbf95156
Richard Lenz <richard-lenz@uiowa.edu>
2017-12-04 23:11:16 -0600

    d

--- vote.php
+++ vote.php
@@ -17,25 +17,28 @@
 	$errorMessage = "";
 	
 	//Check for duplicates in database
-	
+	//Select statement here checking if account id is in db and then do if statements. Make one vote.php
 	
 	if ($isComplete){
 		//everything works
 		
 		session_start();
-		$list_id = $_SESSION['list_id'];
-		$count = $_SESSION['voteCount'];
+		$list_id = $_SESSION['listid'];
 		$accountid = $_SESSION['accountid'];
 		
-        $count++; 
+        
 		//make insert statement	
-		$query = "INSERT INTO vote(list_id,voteCount,accountid) VALUES ($list_id,$count,$accountid)";
-		$_SESSION['voteCount'] = $count;
+		$query = "INSERT INTO vote(list_id,voteCount,accountid) VALUES ($list_id,1,$accountid)";
+		
 			
 		//run insert statement
 		$result = queryDB($query,$db);
 		
-	
+		// update the list table with new count
+		$query = "UPDATE list SET voteCount = voteCount + 1 WHERE id=$list_id";
+
+		//run insert statement
+		$result = queryDB($query,$db);	
 	
 		//send a response back to the called of this php file
 		$response =array();
@@ -63,26 +66,4 @@
 
 
 		
-		//send a response back to the called of this php file
-		$response =array();
-		$response['status'] = 'success';
-		$response['id'] = $id;
-		header('Content-Type: application/json');
-		echo(json_encode($response));
 		
-	} else{
-		//generates an error message to send back to caller
-		ob_start();
-		var_dump($data);
-		$postdump = ob_get_clean();
-		
-		$response = array();
-		$response['status'] = 'error';
-		$response['message'] = $errorMessage . $postdump;
-		header('Content-Type: application/json');
-		echo(json_encode($response));
-	}
-	
-	
-
-?>
